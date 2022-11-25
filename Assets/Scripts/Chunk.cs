655c92b793c3f1fd35a9235f0daf6cb8bb68526f
Henry Post <HenryFBP@gmail.com>
2022-11-25 15:49:17 -0500

    finish part 2

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,37 +9,110 @@ public class Chunk : MonoBehaviour
     [SerializeField] public MeshRenderer meshRenderer;
     [SerializeField] public MeshFilter meshFilter;
 
+    int vertexIndex = 0;
+    List<Vector3> vertices = new List<Vector3>();
+    List<int> triangles = new List<int>();
+    List<Vector2> uvs = new List<Vector2>();
+
+    //tells us if a voxel exists at a particular index
+    bool[,,] voxelMap = new bool[VoxelData.ChunkWidth, VoxelData.ChunkHeight, VoxelData.ChunkDepth];
+
+    bool CheckVoxel(Vector3 pos)
+    {
+
+        int x = Mathf.FloorToInt(pos.x);
+        int y = Mathf.FloorToInt(pos.y);
+        int z = Mathf.FloorToInt(pos.z);
+
+        if ((x < 0 || x > VoxelData.ChunkWidth - 1) ||
+            (y < 0 || y > VoxelData.ChunkHeight - 1) ||
+            (z < 0 || z > VoxelData.ChunkDepth - 1))
+        {
+            return false;
+
+        }
+
+        return voxelMap[x, y, z];
+
+    }
+
+
     void Start()
     {
-        int vertexIdx = 0;
-        List<Vector3> vertices = new List<Vector3>();
-        List<int> triangles = new List<int>();
-        List<Vector2> uvs = new List<Vector2>();
+        PopulateVoxelMap();
+        CreateMeshData();
+        CreateMesh();
+    }
 
-        //TODO I have no idea what exactly this does, but I think it creates a bunch of triangles to get ready to map textures to, on the faces of a cube.
-        //TODO see https://youtu.be/h66IN1Pndd0?list=PLVsTSlfj0qsWEJ-5eMtXsYp03Y9yF1dEn&t=1219
-
-        for (int p = 0; p < 6; p++)
+    void CreateMeshData()
+    {
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
         {
-            for (int i = 0; i < 6; i++)
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
             {
-                int triangleIdx = VoxelData.voxelTriangles[p, i];
-                Vector3 triangleCoords = VoxelData.voxelVertices[triangleIdx];
-
-                vertices.Add(triangleCoords);
-                triangles.Add(vertexIdx);
-
-                uvs.Add(VoxelData.voxelUvs[i]);
-
-                vertexIdx += 1;
+                for (int z = 0; z < VoxelData.ChunkDepth; z++)
+                {
+                    AddVoxelDataToChunk(new Vector3(x, y, z));
+                }
             }
         }
+    }
+
+    void PopulateVoxelMap()
+    {
+        for (int y = 0; y < VoxelData.ChunkHeight; y++)
+        {
+            for (int x = 0; x < VoxelData.ChunkWidth; x++)
+            {
+                for (int z = 0; z < VoxelData.ChunkDepth; z++)
+                {
+                    voxelMap[x, y, z] = true;
+                }
+            }
+        }
+    }
+
+
+    void CreateMesh()
+    {
         Mesh mesh = new Mesh();
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.uv = uvs.ToArray();
         mesh.RecalculateNormals();
         meshFilter.mesh = mesh;
+    }
+
+    //TODO I have no idea what exactly this does, but I think it creates a bunch of triangles to get ready to map textures to, on the faces of a cube.
+    //TODO see https://youtu.be/h66IN1Pndd0?list=PLVsTSlfj0qsWEJ-5eMtXsYp03Y9yF1dEn&t=1219
+    void AddVoxelDataToChunk(Vector3 pos)
+    {
+
+        for (int p = 0; p < 6; p++)
+        {
+
+            if (!CheckVoxel(pos + VoxelData.faceChecks[p]))
+            {
+
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 0]]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 1]]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 2]]);
+                vertices.Add(pos + VoxelData.voxelVerts[VoxelData.voxelTris[p, 3]]);
+                uvs.Add(VoxelData.voxelUvs[0]);
+                uvs.Add(VoxelData.voxelUvs[1]);
+                uvs.Add(VoxelData.voxelUvs[2]);
+                uvs.Add(VoxelData.voxelUvs[3]);
+                triangles.Add(vertexIndex);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 2);
+                triangles.Add(vertexIndex + 1);
+                triangles.Add(vertexIndex + 3);
+                vertexIndex += 4;
+
+            }
+        }
 
     }
+
 }
--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -5,36 +5,54 @@ using UnityEngine;
 public static class VoxelData
 {
 
-    public static readonly Vector3[] voxelVertices =
-    {
-        new Vector3(0,0,0),
-        new Vector3(1,0,0),
-        new Vector3(1,1,0),
-        new Vector3(0,1,0),
-        new Vector3(0,0,1),
-        new Vector3(1,0,1),
-        new Vector3(1,1,1),
-        new Vector3(0,1,1),
-    };
+	public static readonly int ChunkWidth = 32;
+	public static readonly int ChunkHeight = 32;
+	public static readonly int ChunkDepth = 32;
 
-    public static readonly int[,] voxelTriangles = new int[6, 6]
-    {
-        {0,3,1,1,3,2,},//back    face
-        {5,6,4,4,6,7,},//front   face
-        {3,7,2,2,7,6,},//top     face
-        {1,5,0,0,5,4,},//bottom  face
-        {4,7,0,0,7,3,},//top     face
-        {1,2,5,5,2,6,},//top     face
-    };
+	public static readonly Vector3[] voxelVerts = new Vector3[8] {
 
-    // https://youtu.be/h66IN1Pndd0?list=PLVsTSlfj0qsWEJ-5eMtXsYp03Y9yF1dEn&t=1969
-    public static readonly Vector2[] voxelUvs = new Vector2[6]
-    {
-        new Vector2(0,0),
-        new Vector2(0,1),
-        new Vector2(1,0),
-        new Vector2(1,0),
-        new Vector2(0,1),
-        new Vector2(1,1),
-    };
+		new Vector3(0.0f, 0.0f, 0.0f),
+		new Vector3(1.0f, 0.0f, 0.0f),
+		new Vector3(1.0f, 1.0f, 0.0f),
+		new Vector3(0.0f, 1.0f, 0.0f),
+		new Vector3(0.0f, 0.0f, 1.0f),
+		new Vector3(1.0f, 0.0f, 1.0f),
+		new Vector3(1.0f, 1.0f, 1.0f),
+		new Vector3(0.0f, 1.0f, 1.0f),
+
+	};
+
+	public static readonly Vector3[] faceChecks = new Vector3[6] {
+
+		new Vector3(0.0f, 0.0f, -1.0f),
+		new Vector3(0.0f, 0.0f, 1.0f),
+		new Vector3(0.0f, 1.0f, 0.0f),
+		new Vector3(0.0f, -1.0f, 0.0f),
+		new Vector3(-1.0f, 0.0f, 0.0f),
+		new Vector3(1.0f, 0.0f, 0.0f)
+
+	};
+
+	public static readonly int[,] voxelTris = new int[6, 4] {
+
+		// 0 1 2 2 1 3
+		{0, 3, 1, 2}, // Back Face
+		{5, 6, 4, 7}, // Front Face
+		{3, 7, 2, 6}, // Top Face
+		{1, 5, 0, 4}, // Bottom Face
+		{4, 7, 0, 3}, // Left Face
+		{1, 2, 5, 6} // Right Face
+
+	};
+
+	public static readonly Vector2[] voxelUvs = new Vector2[4] {
+
+		new Vector2 (0.0f, 0.0f),
+		new Vector2 (0.0f, 1.0f),
+		new Vector2 (1.0f, 0.0f),
+		new Vector2 (1.0f, 1.0f)
+
+	};
+
+
 }
